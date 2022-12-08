cb483230d0627ffc1e46b9b32f564d11edeeb81b
Micaela Analia Jofre <mkjofre@gmail.com>
2022-12-08 18:23:21 -0300

    mute

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,11 @@
 import MediaPlayer from "./MediaPlayer.js"
+import AutoPlay from "../plugins/AutoPlay.js"
 
 const video = document.querySelector('video')
-const button = document.querySelector('button')
+const button = document.querySelector('#play-pause')
+const unmute = document.querySelector('#unmute')
 
-const player = new MediaPlayer({ el: video })
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] })
 
-button.onclick = () => {
-    console.log('hola2')
-    player.togglePlay()
-}
+button.onclick = () => player.togglePlay()
+unmute.onclick = () => player.toggleMute()
